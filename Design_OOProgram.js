855e40d87e4ca8437a4cc0cee56d50647a15d4f5
naflanmohamed <naflanmohammed611@gmail.com>
2024-02-09 23:29:20 +0500

    added Design_OOProgram.js

---
+++ Design_OOProgram.js
@@ -0,0 +1,21 @@
+class Animal {
+    constructor(color = 'yellow', energy = 100) {
+        this.color = color;
+        this.energy = energy;
+    }
+    isActive() {
+        if(this.energy > 0) {
+            this.energy -= 20;
+            console.log('Energy is decreasing, currently at:', this.energy)
+        } else if(this.energy == 0){
+            this.sleep();
+        }
+    }
+    sleep() {
+        this.energy += 20;
+        console.log('Energy is increasing, currently at:', this.energy)
+    }
+    getColor() {
+        console.log(this.color)
+    }
+}
