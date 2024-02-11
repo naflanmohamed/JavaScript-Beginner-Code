494dd8a030dd7bd82d26b54b27e38006f2e5f654
naflanmohamed <naflanmohammed611@gmail.com>
2024-02-12 00:19:23 +0500

    Created Data_Structure_forEach.js File

--- Data_Structure_forEach.js
+++ Data_Structure_forEach.js
@@ -11,3 +11,19 @@ const veggies = ['onion', 'garlic', 'potato'];
 veggies.forEach( function(veggie, index_1) {
     console.log(`${index_1}. ${veggie}`);
 });
+
+
+const result = [];
+
+const school = {
+    schoolName: 'Fake School',
+    location: '123 Fake St',
+    established: 1999,
+}
+const schoolKey = Object.keys(school);  // Object.keys() returns an array of a given object's own property names, in the same order as we get with a normal loop.
+
+schoolKey.forEach( function(key) {      // forEach() method executes a provided function once for each array element.
+    result.push(key, school[key])       // push() method adds new items to the end of an array, and returns the new length.
+})
+
+console.log(result)
