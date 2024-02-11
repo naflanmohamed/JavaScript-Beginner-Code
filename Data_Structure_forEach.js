ea31ef710df0721eb17ef2aa9fc13b8571a5ff51
naflanmohamed <naflanmohammed611@gmail.com>
2024-02-11 23:53:30 +0500

    Created Data_Structure_forEach.js File

--- Data_Structure_forEach.js
+++ Data_Structure_forEach.js
@@ -1,5 +1,13 @@
 const fruits = ['kiwi','mango','apple','pear'];
 function appendIndex(fruits,index){
-    console(`${index}. ${fruits}`);
+    console.log(`${index}. ${fruits}`);
 }
 fruits.forEach(appendIndex);
+
+console.log('-------------------');
+
+const veggies = ['onion', 'garlic', 'potato'];
+
+veggies.forEach( function(veggie, index_1) {
+    console.log(`${index_1}. ${veggie}`);
+});
